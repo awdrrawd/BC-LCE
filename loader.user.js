f3120f630072ae44084af296e6fd74b0423f1e15
Likulisu <awdrrawd1@gmail.com>
2026-08-25 17:49:55 +0800

    updata

--- loader.user.js
+++ loader.user.js
@@ -5,7 +5,7 @@
 // @version      1.0
 // @description  Liko Club Extensions - horizontal login UI and club extensions for Bondage Club.
 // @author       Likolisu
-// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @include      /^https:\/\/(www\.)?(bondage(projects\.elementfx|-(europe|asia))\.com|bondageeurope\.com)\/R*/
 // @icon         https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Images/PCM_ICON.png
 // @grant        none
 // @run-at       document-end
